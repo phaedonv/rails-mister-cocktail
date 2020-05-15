6089f358ad101930616a51a9db5bd16d506780b7
Phaedon Valasiadis <faival@protonmail.com>
2020-05-15 17:52:15 +0300

    still back

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,6 @@
 class Dose < ApplicationRecord
+  validates :description, :cocktail, :ingredient, presence: true
+  belongs_to :cocktail
+  belongs_to :ingredient
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
